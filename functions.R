8d33a1c309691a544460e910127b0568e0caf08a
nathandpenha <dpenhanathan@yahoo.in>
2018-03-08 21:01:22 +0530

    performances predict

--- functions.R
+++ functions.R
@@ -101,7 +101,7 @@ batsmanAvgRunsGround <- function( name="A Latecut",format){
 
 #* @get /batsmanAvgRunsOpposition
 #* @png(width=400,400)
-batsmanAvgRunsOpposition <- function(name="A Latecut",format){
+batsmanAvgRunsOpposition <- function(name,format){
   Opposition <-Runs <- NULL
   file<-paste("data/",name,"_",format,"_Batting.csv",sep = "")
   batsman <- clean(file)
@@ -127,5 +127,64 @@ BoundariesVsSingles<-function(name, format){
   file<-paste("data/",name,"_",format,"_Batting.csv",sep = "")
   D <- clean(file)
   plot(batsman$Match.No, (batsman$Runs-(batsman$X4s*4 + batsman$X6s*6)))
+}
+
+
+#* @get /batsmanPerfForecast
+#* @png(width=400,400)
+batsmanPerfForecast <- function( name,format) {
+  file<-paste("data/",name,"_",format,"_Batting.csv",sep = "")
+  b <- clean(file)
+  library(lubridate)
+  library(forecast)
+  library(quadprog)
+  library(quantmod)
+  # Read day, month and year
+  date <- dmy(b$Start.Date)
+  runs <- b$Runs
+  
+  # Create a training and a test set
+  # Subset 90 percent of the rows of the time series
+  rows <- length(runs)
+  i <- floor(0.9 * rows)
+  
+  # Note the start/end month and year
+  startMonth = month(date[1])
+  startYear = year(date[1])
+  endMonth = month(date[i])
+  endYear = year(date[i])
+  
+  # Create training set with the 90 percent career 
+  ts.train <- ts(runs, start = c(startYear,startMonth), end = c(endYear,endMonth),frequency=12)
+  
+  
+  # Make a test set with the remaining 10 percent
+  startMonth1 <- month(date[i+1])
+  startYear1 = year(date[i+1])
+  endMonth1 = month(date[rows])
+  endYear1 = year(date[rows])
+  
+  ts.test <- ts(runs, start = c(startYear1,startMonth1), end = c(endYear1,endMonth1),frequency=12)
+  
+  # Fit a Holt Winters Model with the training set
+  fit <-HoltWinters(ts.train)
+  
+  # Forecast based on the model
+  fcast <- forecast(fit)
+  atitle = paste(name,"-","Runs forecast" )
+  plot(fcast,main=atitle,col="blue",lwd=1.5,xlab="Year",ylab="Runs scored")
+  lines(ts.train,col="magenta")
+  
+  # Draw the test set
+  lines(ts.test,col="red",lwd=1.5)
+  
+  vals <- c("forecasted runs","actual runs scored")
+  col1 <- c("blue","red")
+  legend(x="topleft",vals, lty=c(1,1),   
+         lwd=c(1.5,1.5),col=col1,bty="n",cex=0.8)
+  accuracy(fcast,ts.test)
+  
+  
+  mtext("Data source-Courtesy:ESPN Cricinfo", side=1, line=4, adj=1.0, cex=0.8, col="blue")
 }
 
