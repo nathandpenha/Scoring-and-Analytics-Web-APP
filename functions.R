1c2b28a5a05a5ada3731bbd4e6614e5d2d26de0e
nathandpenha <dpenhanathan@yahoo.in>
2018-03-08 15:38:58 +0530

    created own clean function

--- functions.R
+++ functions.R
@@ -6,6 +6,31 @@ cors <-  function(res){
     plumber::forward()    
 }
 
+#* @get /clean
+clean <- function(file) {
+  
+  df <- read.csv(file,stringsAsFactor=FALSE,na.strings=c(NA,"-"))
+  # Remove rows where the batsman 'did not bat' - DNB
+  a <- df$Runs != "DNB"
+  batsman <- df[a,]
+  
+  # Remove rows with 'TDNB'
+  c <- batsman$Runs != "TDNB"
+  batsman <- batsman[c,]
+  
+  # Remove rows with absent
+  d <- batsman$Runs != "absent"
+  batsman <- batsman[d,]
+  
+  # Remove the "* indicating not out
+  batsman$Runs <- as.numeric(gsub("\\*","",batsman$Runs))
+  batsman
+}
+
+
+
+
+
 
 
 #* @get /mean
@@ -42,5 +67,21 @@ StrikeRateVsGround <- function(name,format){
   D<-read.csv(paste("data/",name,"_",format,"_Batting.csv",sep = ""))
   plot(D$Ground,D$SR)
 } 
+
+
+
+
+#* @get /StrikeRateVsGround
+#* @png(width=400,400)
+StrikeRateVsGround <- function(name,format){
+  D<-read.csv(paste("data/",name,"_",format,"_Batting.csv",sep = ""))
+  plot(D$Ground,D$SR)
+} 
+
+#* @get /check
+check<-function(file){
+D<-clean(file)
+D
+}
 
 
